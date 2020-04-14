31b2a55a4a405e7a0766a65a0e603a48b33f5567
Rich Tillis <rich.tillis@learninga-z.com>
2020-04-14 14:50:55 -0700

    A more finalized version of the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,14 +9,36 @@ export class HeaderComponent implements OnInit {
 
   menuOpen: boolean = false;
 
+  socialLinks = [
+    {
+      href: 'https://www.linkedin.com/in/richtillis/',
+      icon: 'fa fa-linkedin'
+    },
+    {
+      href: 'mailto:rich@richtillis.com',
+      icon: 'fa fa-envelope-o'
+    },
+    {
+      href: 'https://github.com/richtillis/',
+      icon: 'fa fa-github'
+    },
+    {
+      href: 'https://twitter.com/richtillis/',
+      icon: 'fa fa-twitter'
+    },
+    {
+      href: 'https://www.stackoverflow.com/users/4577397/richtillis/',
+      icon: 'fa fa-stack-overflow'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  toggleMenu() {
+  toggleHamburgerMenu() {
     this.menuOpen = !this.menuOpen;
-    console.log('Menu open? ', this.menuOpen);
   }
 
 }

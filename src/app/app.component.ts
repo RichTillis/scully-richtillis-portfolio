2682b8c1c43bef27947fec029b8a24fbf89603d9
Rich Tillis <rich.tillis@learninga-z.com>
2021-02-20 17:49:47 -0700

    first go-around of lighthouse related updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { filter } from 'rxjs/operators';
-import { Router, NavigationEnd } from '@angular/router';
+
 
 declare var gtag;
 
@@ -11,13 +10,5 @@ declare var gtag;
 })
 export class AppComponent {
   title = 'richtillis-portfolio-app';
-  constructor(router: Router) {
-    const navEndEvent$ = router.events.pipe(
-      filter(e => e instanceof NavigationEnd)
-    );
-    navEndEvent$.subscribe((e: NavigationEnd) => {
-      gtag('config', 'UA-167882399-1', { 'page_path': e.urlAfterRedirects });
-    });
-  }
 
 }
